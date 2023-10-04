90ad5f2979b701ada96af5e7eb4e94ada0c76b79
steedude <85280987+steedude@users.noreply.github.com>
2023-10-04 16:08:08 +0800

    google登入拿到credential

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { reactive } from 'vue'
+import { reactive, onMounted } from 'vue'
 import { useUserStore } from '@/stores'
 import router from '../router'
 const { loginUser } = useUserStore()
@@ -16,15 +16,34 @@ const login = async () => {
     console.log(error)
   }
 }
+const handleCredentialResponse = (response) => {
+  console.log('Encoded JWT ID token: ' + response.credential)
+}
+
+onMounted(() => {
+  window.google.accounts.id.initialize({
+    client_id: '462578570380-j2ed57fpoqdc19jbilsh44h1ha22e0id.apps.googleusercontent.com',
+    callback: handleCredentialResponse
+  })
+  window.google.accounts.id.renderButton(document.getElementById('buttonDiv'), { locale: 'fr' })
+  window.google.accounts.id.renderButton(document.getElementById('buttonDiv'), {
+    theme: 'outline',
+    size: 'large',
+    width: 200
+  })
+})
 </script>
 
 <template>
-  <div>登入</div>
-  <label for="username">帳號：</label>
-  <input type="text" id="username" v-model="loginData.username" />
-  <label for="password">密碼：</label>
-  <input type="text" id="password" v-model="loginData.password" />
-  <button @click="login">登入</button>
+  <div>
+    <div>登入</div>
+    <label for="username">帳號：</label>
+    <input type="text" id="username" v-model="loginData.username" />
+    <label for="password">密碼：</label>
+    <input type="text" id="password" v-model="loginData.password" />
+    <button @click="login">登入</button>
+  </div>
+  <div id="buttonDiv"></div>
 </template>
 
 <style lang="scss" scoped></style>
